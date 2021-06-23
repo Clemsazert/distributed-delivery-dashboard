ad5f8cd106efa4ebbc29ce8e7f852a3e2b6ebfcc
Clemsazert <clement.didom@gmail.com>
2021-06-23 23:22:21 +0200

    added pages structures

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  Route,
+  Switch,
+  BrowserRouter as Router,
+  Redirect
+} from "react-router-dom";
+
+import { FirstPart } from "./pages/firstPart";
+import { SecondPart } from "./pages/secondPart";
+
+const routes = [
+  {
+    path: "/part1",
+    component: FirstPart,
+    title: "First Part"
+  },
+  {
+    path: "/part2",
+    component: SecondPart,
+    title: "Second Part"
+  }
+];
 
 const App: React.FC = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit <code>src/App.tsx</code> and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </header>
-  </div>
+  <Router>
+    <Switch>
+      {routes.map(({ path, component }) => (
+        <Route key={path} path={path} component={component} />
+      ))}
+      <Redirect to="/part1" />
+    </Switch>
+  </Router>
 );
 
 export default App;
---
+++ src/pages/secondPart.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const SecondPart: React.FC = () => (
+  <div>
+    <h1>Second Part</h1>
+  </div>
+);
